55e3fb7d3b561cbf143d4e7b01bc1de76a3f6ed4
Heriberto Fabian Santiago <fabiansantiago87@gmail.com>
2022-01-27 14:13:32 -0600

    doAnswer para el tratamiento de la respuesta en base a diversas condiciones

--- src/test/java/org/fash/appmockito/ejemplos/service/ExamenServiceImplTest.java
+++ src/test/java/org/fash/appmockito/ejemplos/service/ExamenServiceImplTest.java
@@ -207,7 +207,68 @@ class ExamenServiceImplTest {
         assertThrows(IllegalArgumentException.class, () -> {
             service.guardar(examen);
         });
+    }
 
+    @Test
+    void testDoAnswer() {
+        when(repository.findAll()).thenReturn(Datos.EXAMENES);
+        //when(preguntaRepository.findPreguntasPorExamenId(anyLong())).thenReturn(Datos.PREGUNTAS);
+        //doAnswer se utiliza cuando se quiere realizar un tratamiento de los datos antes de su retorno
+        doAnswer(invocation -> {
+           Long idExamen = invocation.getArgument(0);
+           return idExamen == 5L ? Datos.PREGUNTAS : null;
+        }).when(preguntaRepository).findPreguntasPorExamenId(anyLong());
+
+        Examen examen = service.findExamenPorNombreConPreguntas("Matematicas");
+
+        assertEquals(5L, examen.getId());
+        assertEquals("Matematicas", examen.getNombre());
+        assertTrue(examen.getPreguntas().contains("Geometria"));
+        assertEquals(5, examen.getPreguntas().size());
+
+        verify(preguntaRepository).findPreguntasPorExamenId(anyLong());
+    }
+
+
+    @Test
+    void testGuardarExamenDoAnswer() {
+        //Given
+        Examen newExamen = Datos.EXAMEN;
+        newExamen.setPreguntas(Datos.PREGUNTAS);
+
+        doAnswer(new Answer<Examen>(){
+
+            Long secuencia = 8L;
+            @Override
+            public Examen answer(InvocationOnMock invocation) throws Throwable {
+                Examen examen = invocation.getArgument(0);
+                examen.setId(secuencia++);
+                return examen;
+            }
+        }).when(repository).guardar(any(Examen.class));
+
+        /*  La implementacion anterior de doAnswer remplaza el siguiente script
+        when(repository.guardar(any(Examen.class))).then(new Answer<Examen>(){
+
+            Long secuencia = 8L;
+            @Override
+            public Examen answer(InvocationOnMock invocation) throws Throwable {
+                Examen examen = invocation.getArgument(0);
+                examen.setId(secuencia++);
+                return examen;
+            }
+        });*/
+
+        //When
+        Examen examen = service.guardar(newExamen);
+
+        //Then
+        assertNotNull(examen.getId());
+        assertEquals(8L, examen.getId());
+        assertEquals("Fisica", examen.getNombre());
+
+        verify(repository).guardar(any(Examen.class));
+        verify(preguntaRepository).guardarVarias(anyList());
     }
 }
 
